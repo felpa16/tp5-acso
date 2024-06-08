8a7748781658fdd5d338130387e46a695ce096d3
felpa16 <flaffaye@udesa.edu.ar>
2024-06-08 00:11:10 -0300

    debuggeando

--- pathname.c
+++ pathname.c
@@ -2,11 +2,64 @@
 #include "pathname.h"
 #include "directory.h"
 #include "inode.h"
-#include "test.h"
 #include "diskimg.h"
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
+
+
+char **split(const char *str, const char *delimiter, int *count) {
+    // Copy the input string to avoid modifying the original string
+    char *str_copy = strdup(str);
+    if (str_copy == NULL) {
+        perror("strdup");
+        exit(EXIT_FAILURE);
+    }
+
+    // Count the number of tokens
+    int token_count = 0;
+    char *temp = strdup(str);
+    if (temp == NULL) {
+        perror("strdup");
+        exit(EXIT_FAILURE);
+    }
+    char *token = strtok(temp, delimiter);
+    while (token != NULL) {
+        token_count++;
+        token = strtok(NULL, delimiter);
+    }
+    free(temp);
+
+    // Allocate memory for the array of strings
+    char **result = malloc((token_count + 1) * sizeof(char *));
+    if (result == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    // Split the string and store the tokens in the array
+    int index = 0;
+    token = strtok(str_copy, delimiter);
+    while (token != NULL) {
+        result[index] = strdup(token);
+        if (result[index] == NULL) {
+            perror("strdup");
+            exit(EXIT_FAILURE);
+        }
+        index++;
+        token = strtok(NULL, delimiter);
+    }
+    result[index] = NULL; // Null-terminate the array
+
+    // Set the count of tokens
+    *count = token_count;
+
+    // Free the copied string
+    free(str_copy);
+
+    return result;
+}
 
 /**
  * TODO
