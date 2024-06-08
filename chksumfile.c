ca89fa86ad1a40614013be1bc387ebeab1e8c7a9
felpa16 <flaffaye@udesa.edu.ar>
2024-06-08 10:42:15 -0300

    debuggeando

--- chksumfile.c
+++ chksumfile.c
@@ -57,8 +57,8 @@ int chksumfile_byinumber(struct unixfilesystem *fs, int inumber, void *chksum) {
 }
 
 int chksumfile_bypathname(struct unixfilesystem *fs, const char *pathname, void *chksum) {
-  printf("inumber del chksumfile_bypathname: %i\n", inumber);
   int inumber = pathname_lookup(fs, pathname);
+  printf("inumber del chksumfile_bypathname: %i\n", inumber);
   if (inumber < 0) {
     return inumber;
   }
