4721294d5d8cdd345a307e6afa75646978ec2639
felpa16 <flaffaye@udesa.edu.ar>
2024-06-08 00:07:58 -0300

    pathname.c

--- pathname.c
+++ pathname.c
@@ -2,6 +2,7 @@
 #include "pathname.h"
 #include "directory.h"
 #include "inode.h"
+#include "test.h"
 #include "diskimg.h"
 #include <stdio.h>
 #include <string.h>
@@ -11,6 +12,26 @@
  * TODO
  */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    //Implement code here
-	return 0;
+    if (fs == NULL || pathname == NULL) return -1;
+    int count;
+    const char* delimiter = "/";
+    char** split_strings = split(pathname, delimiter, &count);
+
+    int dirinumber = 0;
+    for (int i = 0; i < count; i++) {
+        struct direntv6 dirent;
+        if (directory_findname(fs, split_strings[i], dirinumber, &dirent) == -1) {
+            for (int j = 0; j < count; j++) {
+                free(split_strings[i]);
+            }
+            free(split_strings);
+            return -1;
+        }
+        dirinumber = dirent.d_inumber;
+    }
+    for (int i = 0; i < count; i++) {
+        free(split_strings[i]);
+    }
+    free(split_strings);
+	return dirinumber;
 }
