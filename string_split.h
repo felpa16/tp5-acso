7f6b361697a753925de856d6e16d0b791330a6a5
felpa16 <101142816+felpa16@users.noreply.github.com>
2024-06-10 20:44:04 -0300

    Entrega Final

--- string_split.h
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-#include <stdlib.h>
-
-/*
-Receives a string and a divisor. Divides the string into smaller strings separated
-by the divisor and returns an array of said strings.
-*/
-char **split(const char *str, const char *delimiter, int *count);
