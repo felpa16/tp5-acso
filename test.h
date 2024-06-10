da3e0002b9c45b874875ad93a0ce670b54ba74ce
felpa16 <101142816+felpa16@users.noreply.github.com>
2024-06-10 20:44:45 -0300

    Entrega Final

--- test.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/*
-Receives a string and a divisor. Divides the string into smaller strings separated
-by the divisor and returns an array of said strings.
-*/
-char **split(const char *str, const char *delimiter, int *count);
